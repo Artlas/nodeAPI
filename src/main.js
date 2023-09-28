64060d9f4bf96da1611ff054066ff142e0993390
Aurélien Bon <aurelien.bon@me.com>
2023-09-28 11:14:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-const db = require('./dbClient')
-db.on("error", (err) => {
-  console.error(err)
-})
 
 app.use(bodyParser.urlencoded({
   extended: false
