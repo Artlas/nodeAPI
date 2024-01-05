9af1b9c3a34dd13443cc835b7a8f773123e6e85a
Arthur Fournier <arthur.g.fournier@gmail.com>
2024-01-05 19:19:27 +0100

    create api staging and prod

--- reload_request.py
+++ reload_request.py
@@ -2,6 +2,7 @@ from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 import os
 import requests
+import sys
 open_id_connect_token = id_token.fetch_id_token(Request(), os.environ["IAP_CLIENT_ID"])
 headers = {
     'Authorization': f'Bearer {open_id_connect_token}',
@@ -9,9 +10,9 @@ headers = {
     'Content-Type': 'application/json'
 }
 json = {
-    'name': 'api',
+    'name': sys.argv[1],
     'image': 'ghcr.io/artlas/api',
-    'tag': 'latest'
+    'tag': sys.argv[2]
 } 
 x = requests.post("https://docker.fournierfamily.ovh/to_reload",headers=headers, json=json, timeout=600)
 
