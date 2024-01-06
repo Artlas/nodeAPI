92c592357cb1f3a70ebeafacefa68d1ccdd808f3
Nico DL <78737983+Nicodl05@users.noreply.github.com>
2024-01-06 20:17:24 +0100

    [main]: chore: implement eslint

--- src/main.js
+++ src/main.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const apiUp = require("./routes/apiup");
-const userRouter = require("./routes/user");
-const jwt = require("./auth/jwt");
-const NodeRSA = require("node-rsa");
-const fs = require("fs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiUp = require('./routes/apiup');
+const userRouter = require('./routes/user');
+const jwt = require('./auth/jwt');
+const NodeRSA = require('node-rsa');
+const fs = require('fs');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const port = process.env.PORT || 3000;
-const corsObject= cors({
-  origin: ['http://localhost:3000','https://staging.fournierfamily.ovh', 'https://fournierfamily.ovh'],
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-})
+const corsObject = cors({
+    origin: ['http://localhost:3000', 'https://staging.fournierfamily.ovh', 'https://fournierfamily.ovh'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+});
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(corsObject);
-app.options("/user/*", corsObject);
-app.get("/", apiUp);
-app.use("/user", userRouter);
-app.get("/authorized", function (req, res) {
-  try {
-    let value = jwt.getToken(req.headers.token);
-    res.status(201).json(value);
-  } catch (err) {
-    res.status(401).json(err);
-  }
+app.options('/user/*', corsObject);
+app.get('/', apiUp);
+app.use('/user', userRouter);
+app.get('/authorized', function (req, res) {
+    try {
+        let value = jwt.getToken(req.headers.token);
+        res.status(201).json(value);
+    } catch (err) {
+        res.status(401).json(err);
+    }
 });
 
-if (!fs.existsSync("private.key")) {
-  const newkey = new NodeRSA({ b: 512 });
-  newkey.generateKeyPair();
-  const private = newkey.exportKey("private");
-  const public = newkey.exportKey("public");
-  fs.writeFileSync("private.key", private);
-  fs.writeFileSync("public.key", public);
-  console.log("New keys generated!");
+if (!fs.existsSync('private.key')) {
+    const newkey = new NodeRSA({ b: 512 });
+    newkey.generateKeyPair();
+    const private = newkey.exportKey('private');
+    const public = newkey.exportKey('public');
+    fs.writeFileSync('private.key', private);
+    fs.writeFileSync('public.key', public);
+    console.log('New keys generated!');
 }
 
 const server = app.listen(port, (err) => {
-  if (err) throw err;
-  console.log("Server listening the port " + port);
+    if (err) throw err;
+    console.log('Server listening the port ' + port);
 });
 
 module.exports = server;
