c3766e4cdf45a88a6e3aa2972cbe57459516d226
Nico DL <78737983+Nicodl05@users.noreply.github.com>
2024-01-08 12:55:41 +0100

    [main]: feat: implement checkUserExists
    Implementation of checkUserExists
    used for the signup function instead of route /connect

--- src/Database/userDB.js
+++ src/Database/userDB.js
@@ -1,60 +1,80 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const url = process.env.MONGODBURL ||  "mongodb://localhost:27017/";
-const bdd = 'Artlas'
+const url = process.env.MONGODBURL || 'mongodb://localhost:27017/';
+const bdd = 'Artlas';
 
 const client = new MongoClient(url, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function checkUser(email, id, password) {
     try {
         await client.connect();
         const db = client.db(bdd);
-        let query
-        if(id!=null){
-            query = {'id':id}
-        }else{
-            query = {'mail': email}
+        let query;
+        if (id != null) {
+            query = { id: id };
+        } else {
+            query = { mail: email };
         }
         const collection = db.collection('User');
-        let user = await collection.findOne(query)
-        if (user==null) {
-            return {'error':'User not found'};
-        }else{
+        let user = await collection.findOne(query);
+        if (user == null) {
+            return { error: 'User not found' };
+        } else {
             if (user.password !== password) {
-                return {'error':'Password does not match'};
-            }else{
+                return { error: 'Password does not match' };
+            } else {
                 return user;
             }
         }
     } catch (err) {
-        console.log(err)
+        console.log(err);
         throw err;
     } finally {
         if (client) client.close();
     }
 }
+async function checkUserExists(email, id) {
+    try {
+        await client.connect();
+        const db = client.db(bdd);
+        let query;
+        if (id != null) {
+            query = { id: id };
+        } else {
+            query = { mail: email };
+        }
+        const collection = db.collection('User');
+        let user = await collection.findOne(query);
+        return { userExists: user != null };
+    } catch (err) {
+        console.log(err);
+        return { userExists: false, error: 'An error occurred', details: err };
+    } finally {
+        await client.close();
+    }
+}
 
 async function createUser(id, mail, password, firstName, lastName, birthdate, address) {
     try {
-        await client.connect()
-        const db = client.db(bdd)
-        const collection = db.collection('User')
-        let query = {'mail': mail}
-        let user = await collection.findOne(query)
-        if (user!=null) {
-            return {'error':'mail already used'}
-        }else{
-            query = {'id': id}
-            user = await collection.findOne(query)
-            if (user!=null) {
-                return {'error':'mail already used'}
-            }else{
+        await client.connect();
+        const db = client.db(bdd);
+        const collection = db.collection('User');
+        let query = { mail: mail };
+        let user = await collection.findOne(query);
+        if (user != null) {
+            return { error: 'mail already used' };
+        } else {
+            query = { id: id };
+            user = await collection.findOne(query);
+            if (user != null) {
+                return { error: 'mail already used' };
+            } else {
                 const newUser = {
                     id: id,
                     mail: mail,
@@ -63,9 +83,9 @@ async function createUser(id, mail, password, firstName, lastName, birthdate, ad
                     lastName: lastName,
                     birthdate: birthdate,
                     address: address,
-                    permission: 'user'
-                }
-                console.log(newUser)
+                    permission: 'user',
+                };
+                console.log(newUser);
                 const result = await collection.insertOne(newUser);
                 return result;
             }
@@ -77,16 +97,16 @@ async function createUser(id, mail, password, firstName, lastName, birthdate, ad
     }
 }
 
-async function updateUser(id,mail,password,firstName,lastName,birthdate,address,permission){
+async function updateUser(id, mail, password, firstName, lastName, birthdate, address, permission) {
     try {
         await client.connect();
         const db = client.db(bdd);
         const collection = db.collection('User');
-        let query = {'id':id,'mail': mail}
+        let query = { id: id, mail: mail };
         const user = await collection.findOne(query);
-        if (user==null) {
-            return {'error':'User not found'};
-        }else{
+        if (user == null) {
+            return { error: 'User not found' };
+        } else {
             const newUser = {
                 id: id,
                 mail: mail,
@@ -95,9 +115,9 @@ async function updateUser(id,mail,password,firstName,lastName,birthdate,address,
                 lastName: lastName,
                 birthdate: birthdate,
                 address: address,
-                permission: permission
-            }
-            const result = await collection.updateOne(query,{$set: newUser});
+                permission: permission,
+            };
+            const result = await collection.updateOne(query, { $set: newUser });
             return result;
         }
     } catch (err) {
@@ -105,34 +125,32 @@ async function updateUser(id,mail,password,firstName,lastName,birthdate,address,
     } finally {
         if (client) client.close();
     }
-
 }
-async function deleteUser(mail,password){
+async function deleteUser(mail, password) {
     try {
         await client.connect();
         const db = client.db(bdd);
         const collection = db.collection('User');
-        let query = {'mail': mail}
+        let query = { mail: mail };
         const user = await collection.findOne(query);
-        if (user==null) {
-            return {'error':'User not found'};
-        }else if(user.password==password){
+        if (user == null) {
+            return { error: 'User not found' };
+        } else if (user.password == password) {
             const result = await collection.deleteOne(query);
             return result;
-        }else{
-            return {'error':'Bad password'}
+        } else {
+            return { error: 'Bad password' };
         }
     } catch (err) {
         return err;
     } finally {
         if (client) client.close();
     }
-
-
 }
 module.exports = {
     checkUser,
     createUser,
     updateUser,
     deleteUser,
-}
+    checkUserExists,
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,128 +1,170 @@
-const express = require('express')
-const mongodb = require('../Database/userDB')
-const jwt = require('../auth/jwt')
-const user = express.Router()
-user.get('/connect',async(req,resp)=>{
-    if((req.body.mail!=null || req.body.id!=null) && req.body.password!=null){
-        try{
-            let user = await mongodb.checkUser(req.body.mail,req.body.id,req.body.password)
-            if(user.mail!=null){
-                let token = jwt.createToken({'userdata':{'id':user.id,'permission':user.permission}})
-                resp.status(201).json(
-                    {'user':{
-                        'token':token,
-                        'id':user.id,
-                        'mail':user.mail,
-                        'firstName':user.firstName,
-                        'lastName':user.lastName,
-                        'birthdate':user.birthdate,
-                        'address':user.address,
-                        'image':user.image,
-                        'permission':user.permission
-                    }})
-            } else {
-                resp.status(401).json(user)
-            }
-        }catch(error){
-            console.log(error)
-            resp.status(500).send(error)
-        }
-
-    } else {
-        resp.status(400).json({error: "Bad request"})
-    }
-})
-user.post('/connect',async(req,resp)=>{
-    if((req.body.mail!=null || req.body.id!=null) && req.body.password!=null){
-        try{
-            let user = await mongodb.checkUser(req.body.mail,req.body.id,req.body.password)
-            if(user.mail!=null){
-                let token = jwt.createToken({'userdata':{'id':user.id,'permission':user.permission}})
-                resp.status(201).json(
-                    {'user':{
-                        'token':token,
-                        'id':user.id,
-                        'mail':user.mail,
-                        'firstName':user.firstName,
-                        'lastName':user.lastName,
-                        'birthdate':user.birthdate,
-                        'address':user.address,
-                        'image':user.image,
-                        'permission':user.permission
-                    }})
-            } else {
-                resp.status(401).json(user)
-            }
-        }catch(error){
-            console.log(error)
-            resp.status(500).send(error)
-        }
-
-    } else {
-        resp.status(400).json({error: "Bad request"})
-    }
-})
-user.post('/add',async(req,resp)=>{
-    if(req.body.id!=null && req.body.mail!=null && req.body.password!=null && req.body.firstName!=null && req.body.lastName!=null && req.body.birthdate!=null && req.body.address!=null){
-        try{
-            let user = await mongodb.createUser(req.body.id, req.body.mail, req.body.password, req.body.firstName, req.body.lastName, req.body.birthdate, req.body.address, req.body.image)
-            if(user){
-                resp.status(201).json(user)
-            }
-            else{
-                resp.status(401).json(user)
-            }
-        }catch(error){
-            resp.status(500).json({error})
-        }
-    }else{
-        resp.status(400).json({error: "Bad request"})
-    }
-})
-user.put('/update',async(req,resp)=>{
-    if(req.body.id!=null && req.body.mail!=null && req.body.password!=null && req.body.firstName!=null && req.body.lastName!=null && req.body.birthdate!=null && req.body.address!=null && req.body.image!=null){
+const express = require('express');
+const mongodb = require('../Database/userDB');
+const jwt = require('../auth/jwt');
+const user = express.Router();
+user.get('/connect', async (req, resp) => {
+    if ((req.body.mail != null || req.body.id != null) && req.body.password != null) {
         try {
-            let value = jwt.getToken(req.headers.token)
-            if(value.userdata.permission == 'admin' || value.userdata.id==req.body.id){
-                let user = await mongodb.updateUser(req.body.id, req.body.mail, req.body.password, req.body.firstName, req.body.lastName, req.body.birthdate, req.body.address,req.body.image, value.userdata.permission)
-                if(user){
-                    resp.status(201).json(user)
-                }
-                else{
-                    resp.status(401).json(user)
-                }
+            let user = await mongodb.checkUser(req.body.mail, req.body.id, req.body.password);
+            if (user.mail != null) {
+                let token = jwt.createToken({ userdata: { id: user.id, permission: user.permission } });
+                resp.status(201).json({
+                    user: {
+                        token: token,
+                        id: user.id,
+                        mail: user.mail,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        birthdate: user.birthdate,
+                        address: user.address,
+                        image: user.image,
+                        permission: user.permission,
+                    },
+                });
             } else {
-                resp.status(401).json({error: "Unauthorized"})
+                resp.status(401).json(user);
             }
         } catch (error) {
-            console.log(error)
-            resp.status(500).json({error})
+            console.log(error);
+            resp.status(500).send(error);
         }
-    }else{
-        resp.status(400).json({error: "Bad request"})
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
     }
-})
-user.delete('/delete',async(req,resp)=>{
-    if((req.body.mail!=null || req.body.id!=null) && req.body.password!=null && req.headers.token!=null){
+});
+user.post('/connect', async (req, resp) => {
+    if ((req.body.mail != null || req.body.id != null) && req.body.password != null) {
         try {
-            let value = jwt.getToken(req.headers.token)
-            if(value.userdata.permission == 'admin'){
-                let user = await mongodb.deleteUser(req.body.mail, req.body.password)
-                if(user){
-                    resp.status(201).json(user)
-                }
-                else{
-                    resp.status(401).json(user)
-                }
+            let user = await mongodb.checkUser(req.body.mail, req.body.id, req.body.password);
+            if (user.mail != null) {
+                let token = jwt.createToken({ userdata: { id: user.id, permission: user.permission } });
+                resp.status(201).json({
+                    user: {
+                        token: token,
+                        id: user.id,
+                        mail: user.mail,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        birthdate: user.birthdate,
+                        address: user.address,
+                        image: user.image,
+                        permission: user.permission,
+                    },
+                });
             } else {
-                resp.status(401).json({error: "Unauthorized"})
+                resp.status(401).json(user);
             }
         } catch (error) {
-            resp.status(500).json({error})
+            console.log(error);
+            resp.status(500).send(error);
         }
-    }else{
-        resp.status(400).json({error: "Bad request"})
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
     }
-})
+});
+user.post('/check', async (req, resp) => {
+    if (req.body.mail != null || req.body.id != null) {
+        try {
+            let response = await mongodb.checkUserExists(req.body.mail, req.body.id);
+            if (response.userExists) {
+                resp.status(200).json({ message: 'User exists' });
+            } else {
+                resp.status(404).json({ message: 'User not found' });
+            }
+        } catch (error) {
+            console.log(error);
+            resp.status(500).send({ error: 'Internal server error', details: error });
+        }
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
+    }
+});
 
-module.exports = user
+user.post('/add', async (req, resp) => {
+    if (
+        req.body.id != null &&
+        req.body.mail != null &&
+        req.body.password != null &&
+        req.body.firstName != null &&
+        req.body.lastName != null &&
+        req.body.birthdate != null &&
+        req.body.address != null
+    ) {
+        try {
+            let user = await mongodb.createUser(req.body.id, req.body.mail, req.body.password, req.body.firstName, req.body.lastName, req.body.birthdate, req.body.address, req.body.image);
+            if (user) {
+                resp.status(201).json(user);
+            } else {
+                resp.status(401).json(user);
+            }
+        } catch (error) {
+            resp.status(500).json({ error });
+        }
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
+    }
+});
+user.put('/update', async (req, resp) => {
+    if (
+        req.body.id != null &&
+        req.body.mail != null &&
+        req.body.password != null &&
+        req.body.firstName != null &&
+        req.body.lastName != null &&
+        req.body.birthdate != null &&
+        req.body.address != null &&
+        req.body.image != null
+    ) {
+        try {
+            let value = jwt.getToken(req.headers.token);
+            if (value.userdata.permission == 'admin' || value.userdata.id == req.body.id) {
+                let user = await mongodb.updateUser(
+                    req.body.id,
+                    req.body.mail,
+                    req.body.password,
+                    req.body.firstName,
+                    req.body.lastName,
+                    req.body.birthdate,
+                    req.body.address,
+                    req.body.image,
+                    value.userdata.permission
+                );
+                if (user) {
+                    resp.status(201).json(user);
+                } else {
+                    resp.status(401).json(user);
+                }
+            } else {
+                resp.status(401).json({ error: 'Unauthorized' });
+            }
+        } catch (error) {
+            console.log(error);
+            resp.status(500).json({ error });
+        }
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
+    }
+});
+user.delete('/delete', async (req, resp) => {
+    if ((req.body.mail != null || req.body.id != null) && req.body.password != null && req.headers.token != null) {
+        try {
+            let value = jwt.getToken(req.headers.token);
+            if (value.userdata.permission == 'admin') {
+                let user = await mongodb.deleteUser(req.body.mail, req.body.password);
+                if (user) {
+                    resp.status(201).json(user);
+                } else {
+                    resp.status(401).json(user);
+                }
+            } else {
+                resp.status(401).json({ error: 'Unauthorized' });
+            }
+        } catch (error) {
+            resp.status(500).json({ error });
+        }
+    } else {
+        resp.status(400).json({ error: 'Bad request' });
+    }
+});
+
+module.exports = user;
