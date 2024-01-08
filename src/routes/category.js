f54af8aaf53d6c08963045de070783a48de0d112
Aurélien Bon <aurelien.bon@me.com>
2024-01-08 18:45:04 +0100

    Update category.js

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,7 +3,7 @@ const mongodb = require('../Database/categoryDB')
 const jwt = require('../auth/jwt')
 const category = express.Router()
 
-category.post('/get',async(req,resp)=>{
+category.post('/getCat',async(req,resp)=>{
     try{
         let category = await mongodb.getCategory(req.body.category)
         if(category){
