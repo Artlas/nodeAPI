433605894b22b62abb8fee1b19806e5ce27cbeca
Aurélien Bon <aurelien.bon@me.com>
2024-01-30 11:24:30 +0100

    Feat: add oeuvre to database

--- src/Database/oeuvreDB.js
+++ src/Database/oeuvreDB.js
@@ -31,6 +31,26 @@ async function getIdOeuvre(id){
     }
 }
 
+async function getAuthorOeuvre(author){
+    try {
+        await client.connect();
+        const db = client.db(bdd);
+        let query = {'author':author}
+        const collection = db.collection('Oeuvre');
+        let oeuvre = await collection.find(query).toArray()
+        if (oeuvre==null) {
+            return {'error':'Oeuvre not found'};
+        }else{
+            return oeuvre;
+        }
+    } catch (err) {
+        console.log(err)
+        throw err;
+    } finally {
+        if (client) client.close();
+    }
+}
+
 async function getAllOeuvre(){
     try {
         await client.connect();
@@ -41,6 +61,25 @@ async function getAllOeuvre(){
             return {'error':'Oeuvre not found'};
         }else{
             return oeuvre;
+        }
+    } catch (err) {
+        console.log(err)
+        throw err;
+    } finally {
+        if (client) client.close();
+    }
+}
+
+async function getAllIdOeuvre(){
+    try {
+        await client.connect();
+        const db = client.db(bdd);
+        const collection = db.collection('Oeuvre');
+        let oeuvre = await collection.find().toArray()
+        if (oeuvre==null) {
+            return {'error':'Oeuvre not found'};
+        }else{
+            return oeuvre.map((oeuvre)=>oeuvre._id);
         }
     } catch (err) {
         console.log(err)
@@ -76,9 +115,92 @@ async function getCatOeuvre(category,subCategory){
 
 }
 
+async function likePost(postId,userId){
+    try {
+        await client.connect();
+        const db = client.db(bdd);
+        let query = {'_id':parseInt(postId)}
+        const collection = db.collection('Oeuvre');
+        let oeuvre = await collection.findOne(query);
+        if (oeuvre==null) {
+            return {'error':'Oeuvre not found'};
+        }else{
+            query = {'id':userId}
+            const collection2 = db.collection('User');
+            let user = await collection2.findOne(query);
+            if (user==null) {
+                return {'error':'User not found'};
+            }else{
+                let query3 = {'_id':parseInt(postId)}
+                const collection3 = db.collection('Oeuvre');
+                let userLike = oeuvre.likes.find((element) => element == userId);
+                if(userLike!=null){
+                    return {'error':'Oeuvre already like by user'}
+                }
+                oeuvre.likes.push(userId);
+                let oeuvre2 = await collection3.updateOne(query3,{$set:{likes:oeuvre.likes}})
+                return oeuvre2;
+            }
+        }
+    } catch (err) {
+        console.log(err)
+        throw err;
+    } finally {
+        if (client) client.close();
+    }
+
+}
+
+async function addOeuvre(title, description, author, category, subCategory, illustration, video,postDate,releaseDate, isMediaTypeImages, likeCount,toSell,price,linkToBuy,canTchat){
+    try {
+        await client.connect();
+        const db = client.db(bdd);
+        let newOeuvre = {
+            title: title,
+            description: description,
+            category: category,
+            subCategory: subCategory,
+            illustration: illustration,
+            video: video,
+            postDate:postDate ,
+            releaseDate: releaseDate,
+            isMediaTypeImages: isMediaTypeImages,
+            author: author,
+            likeCount: likeCount,
+            toSell: toSell,
+            price: price,
+            linkToBuy: linkToBuy,
+            canTchat: canTchat,
+        };
+        const collection2 = db.collection('Art');
+        let query = {'name':category}
+        let artCat=await  collection2.findOne(query)
+        if(artCat==null){
+            return {'error':'Category not found'};
+        }
+        query = {'name':category,'subcategories':{$elemMatch:{'name':subCategory}}};
+        let artSubCat=await  collection2.findOne(query)
+        if(artSubCat==null){
+            return {'error':'SubCategory not found'};
+        }
+        const collection = db.collection('Oeuvre');
+        let result = await collection.insertOne(newOeuvre);
+        return result;
+    } catch (err) {
+        console.log(err)
+        throw err;
+    }finally{
+        if(client) client.close();
+    }
+}
+
 
 module.exports = {
     getIdOeuvre,
     getAllOeuvre,
-    getCatOeuvre
+    getAllIdOeuvre,
+    getCatOeuvre,
+    getAuthorOeuvre,
+    likePost,
+    addOeuvre
 }
--- src/routes/oeuvre.js
+++ src/routes/oeuvre.js
@@ -6,7 +6,7 @@ const oeuvre = express.Router()
 oeuvre.post('/getId',async(req,resp)=>{
     if(req.body.id!=null){
         try{
-            jwt.getToken(req.headers.token)
+            // jwt.getToken(req.headers.token)
             let oeuvre = await mongodb.getIdOeuvre(req.body.id)
             if(oeuvre){
                 resp.status(201).json(oeuvre)
@@ -21,11 +21,42 @@ oeuvre.post('/getId',async(req,resp)=>{
         resp.status(400).json({error: "Bad request"})
     }
 })
-
+oeuvre.post('/getAuthorOeuvre',async(req,resp)=>{
+    if(req.body.author!=null){
+        try{
+            // jwt.getToken(req.headers.token)
+            let oeuvre = await mongodb.getAuthorOeuvre(req.body.author)
+            if(oeuvre){
+                resp.status(201).json(oeuvre)
+            }
+            else{
+                resp.status(401).json(oeuvre)
+            }
+        }catch(error){
+            resp.status(500).json({error})
+        }
+    }else{
+        resp.status(400).json({error: "Bad request"})
+    }
+})
 oeuvre.post('/getAll',async(req,resp)=>{
     try{
-        jwt.getToken(req.headers.token)
+        // jwt.getToken(req.headers.token)
         let oeuvre = await mongodb.getAllOeuvre()
+        if(oeuvre){
+            resp.status(201).json(oeuvre)
+        }
+        else{
+            resp.status(401).json(oeuvre)
+        }
+    }catch(error){
+        resp.status(500).json({error})
+    }
+})
+oeuvre.post('/getAllId',async(req,resp)=>{
+    try{
+        // jwt.getToken(req.headers.token)
+        let oeuvre = await mongodb.getAllIdOeuvre()
         if(oeuvre){
             resp.status(201).json(oeuvre)
         }
@@ -39,7 +70,7 @@ oeuvre.post('/getAll',async(req,resp)=>{
 oeuvre.post('/getCat',async(req,resp)=>{
     if(req.body.category!=null && req.body.subCategory!=null){
         try{
-            jwt.getToken(req.headers.token)
+            // jwt.getToken(req.headers.token)
             let oeuvre = await mongodb.getCatOeuvre(req.body.category,req.body.subCategory)
             if(oeuvre){
                 resp.status(201).json(oeuvre)
@@ -55,6 +86,55 @@ oeuvre.post('/getCat',async(req,resp)=>{
     }
 })
 
+oeuvre.post('/likePost', async (req,resp)=>{
+    if(req.body.postId!=null && req.body.userId!=null){
+        // jwt.getToken(req.headers.token)
+        // if(value.userdata.permission == 'admin' || value.userdata.id==req.body.id){
+        try{
+            let oeuvre = await mongodb.likePost(req.body.postId,req.body.userId)
+            if(oeuvre){
+                resp.status(201).json(oeuvre)
+            }
+            else{
+                resp.status(401).json(oeuvre)
+            }
+        }catch(error){
+            resp.status(500).json({error})
+        }
+        // } else {
+        //     resp.status(401).json({error: "Unauthorized"})
+        // }
+    }else{
+        resp.status(400).json({error: "Bad request"})
+    }
+})
 
+oeuvre.post('/addOeuvre',async (req,resp)=>{
+    if(
+        req.body.title!=null &&
+        req.body.description!=null &&
+        req.body.author!=null &&
+        req.body.category!=null &&
+        req.body.subCategory!=null &&
+        (req.body.illustration!=null ||req.body.video!=null) &&
+        req.body.isMediaTypeImages!=null &&
+        req.body.likeCount!=null
+    ){
+        try{
+            // jwt.getToken(req.headers.token)
+            let oeuvre = await mongodb.addOeuvre(req.body.title, req.body.description, req.body.author, req.body.category, req.body.subCategory, req.body.illustration, req.body.video, req.body.isMediaTypeImages, req.body.likeCount,req.body.toSell,req.body.price,req.body.linkToBuy,req.body.canTchat)
+            if(oeuvre){
+                resp.status(201).json(oeuvre)
+            }
+            else{
+                resp.status(401).json(oeuvre)
+            }
+        }catch(error){
+            resp.status(500).json({error})
+        }
+    }else{
+        resp.status(400).json({error: "Bad request"})
+    }
+})
 
 module.exports = oeuvre
