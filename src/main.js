7204242abe0e6917e70af35c1ea84097a2e13cdd
Aurélien Bon <aurelien.bon@me.com>
2024-02-08 16:19:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -29,8 +29,6 @@ app.use('/oeuvre',oeuvreRouter);
 app.options('/oeuvre/*', corsObject);
 app.use('/category',categoryRouter);
 app.options('/category/*', corsObject);
-let bool = "false"
-console.log(bool.toLowerCase()==="true")
 
 
 app.get('/authorized', function (req, res) {
